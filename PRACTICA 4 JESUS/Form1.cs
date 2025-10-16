a85304b150dbe7d6c490da5398c7788b7dcddcb0
jesusalbertohdz <jesusalbertoh157@gmail.com>
2025-10-16 10:27:08 -0500

    Add files via upload

---
+++ PRACTICA 4 JESUS/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRACTICA_4_JESUS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c, d;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            c = int.Parse(textBox3.Text);
+            d = int.Parse(textBox4.Text);
+
+            textBox5.Text = d.ToString();
+            textBox6.Text = c.ToString();
+            textBox7.Text = b.ToString();
+            textBox8.Text = a.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
